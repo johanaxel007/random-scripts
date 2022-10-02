133f95fbc11c76b5c76892cf51f9e836d8d70c51
johanaxel007 <axel.van.gestel@hotmail.be>
2022-10-02 15:22:19 +0200

    Updated scripts

---
+++ [FFmpeg] Merge mp4 files - recode to h265 [veryslow] [10bit].bat
@@ -0,0 +1,2 @@
+(for /f "delims=" %%A in ('dir /b /a-d *.mp4') do @echo file '%%A') > "list.txt"
+ffmpeg -safe 0 -f concat -i list.txt -pix_fmt yuv420p10le -c:v libx265 -preset veryslow -c:a copy output_h265_veryslow_10bit.mp4
